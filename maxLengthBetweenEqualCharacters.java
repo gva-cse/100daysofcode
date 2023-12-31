f6ec3ee8ffcddf9acbb0b1dfe7b8512222c2b012
Vanshika Gupta <114987374+gva-cse@users.noreply.github.com>
2023-12-31 12:13:27 +0530

    Create maxLengthBetweenEqualCharacters.java

---
+++ maxLengthBetweenEqualCharacters.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxLengthBetweenEqualCharacters(String s) {
+        int maxDistance = -1;
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = s.length() - 1; j > i; j--) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    maxDistance = Math.max(maxDistance, j - i - 1);
+                    break;
+                }
+            }
+        }
+        return maxDistance;
+    }
+}
